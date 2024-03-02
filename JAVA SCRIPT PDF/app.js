08f832e0db1f060aa2403e05e9edc833e4c2e697
Abdulmoiztahir <154448657+Abdulmoiztahir@users.noreply.github.com>
2024-03-02 18:31:55 +0500

    first commit

---
+++ JAVA SCRIPT PDF/app.js
@@ -0,0 +1,126 @@
+// CHAPTER 1
+// ques no 1)
+// alert("hello");
+
+// // ques no 2)
+// alert("Error!Please enter a valid password.");                
+
+// // ques no 3)
+// alert("welcome to js land... \n happy coding!");
+
+// // ques no 4)
+// alert("welcome to js land...");
+
+// alert("happy coding! \n prevent ");
+
+// // ques no 5)
+// alert("helllo... i can run js through my web browser's console");
+
+
+// // ques no 6)
+// alert("hello my name is moiz");
+
+// ques no 7)
+
+//  answer is (c);
+
+// CHAPTER 2
+// ques no 1)
+var username = 'moiz';
+console.log(username);
+// ques no 2)
+
+var myName = 'moiz tahir';
+console.log(myName);
+
+// ques no 3)
+// var message = "hello world";
+// alert(message);
+
+// // ques no 4)
+// var studentBio = 'moiz';
+// alert(studentBio);
+// studentBio = '15years';
+// alert(studentBio);
+// studentBio = 'certified mobile application development';
+// alert(studentBio);
+
+// // ques no 5)
+var food = 'pizza \n pizz \n piz \n pi \n p';
+alert(food);
+
+// // ques no 6)
+var email ='my email address is' + 'moiztahir009@gmail.com';
+alert(email);
+
+// ques no 7)
+var book ="I am trying to learn frm the book A smarter way to learn javascript";
+alert(book)
+  
+// // ques no 8)
+var text ='yah i can write html through usinh javascript'
+document.write(text)
+
+
+// // ques no 9)
+var design = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(design)
+
+// CHAPTER NO 3
+
+// ques no 1)
+var age ='i am 19 years old '
+alert(age)
+
+// ques no 2)
+// var visit = 'you have visite this website  times'+'15';
+// alert(visit)
+// visit=visit+1
+// alert(visit)
+// visit=visit+ 1
+// console.log(visit)
+ 
+// ques no 3)
+var birthyear =2004
+document.write(birthyear)
+
+// ques no 4)
+var visitorName ='John Doe'
+var quantity = ' ordered 5'
+var productTitle =' T-shirts (s)'
+var storeName =' XYZ Clothing store '
+document.write(visitorName + quantity +productTitle+storeName )
+
+// CHAPTER NO 4
+// question no 1)
+var a = 'hello';
+var b = 'hi';
+var c = 6;
+
+// ques no 2)
+// legal variable names
+// var _mac = 'opp'
+// var $bag = 'right'
+// var blockLetter = 'left'
+// var blue3 = 'colour'
+// var wha1t = 'why'
+// // illegal variable name
+// var 123 = 'to'
+// var @guod ='o'
+// var #why = 'hi'
+// var 1dgjy = 'rop'
+// var 'ghuyj' = 'vhu'
+
+// ques no 3 a)
+// var heading = 'RULES FOR NAMING JS VARIABLES '
+// document.write(heading)
+// b
+var $your_name69variable = 'that is_my$118variable'
+document.write($your_name69variable);
+// c
+var just = 'be begin with letter , $ or _'
+document.write(just);
+// d
+// java script is a case sensitive
+// e
+// variable name should not be keyword for example var alert = 'pushpa'
