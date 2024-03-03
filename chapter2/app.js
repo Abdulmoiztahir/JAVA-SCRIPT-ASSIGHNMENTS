c70556bb0a45d02a58e496827cdd029eceb526b0
Abdulmoiztahir <154448657+Abdulmoiztahir@users.noreply.github.com>
2024-03-03 19:50:01 +0500

    first commit

---
+++ chapter2/app.js
@@ -0,0 +1,41 @@
+// CHAPTER 2
+// ques no 1)
+var username = 'moiz';
+console.log(username);
+// ques no 2)
+
+var myName = 'moiz tahir';
+console.log(myName);
+
+// ques no 3)
+// var message = "hello world";
+// alert(message);
+
+// // ques no 4)
+// var studentBio = 'moiz';
+// alert(studentBio);
+// studentBio = '15years';
+// alert(studentBio);
+// studentBio = 'certified mobile application development';
+// alert(studentBio);
+
+// // ques no 5)
+var food = 'pizza \n pizz \n piz \n pi \n p';
+alert(food);
+
+// // ques no 6)
+var email ='my email address is' + 'moiztahir009@gmail.com';
+alert(email);
+
+// ques no 7)
+var book ="I am trying to learn frm the book A smarter way to learn javascript";
+alert(book)
+  
+// // ques no 8)
+var text ='yah i can write html through usinh javascript'
+document.write(text)
+
+
+// // ques no 9)
+var design = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(design)
